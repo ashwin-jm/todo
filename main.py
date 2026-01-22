1377869e4e3893c3b0a00af45d07321b48f4b715
Ashwin Joseph Manthara <ashwin25112000@gmail.com>
2026-01-22 15:25:41 +0530

    add main function

--- main.py
+++ main.py
@@ -1,50 +1,55 @@
-print("To Do List Application")
+def main():
+    print("To Do List Application")
 
-user_tasks = []
+    user_tasks = []
 
-while True:
-    print("1. Add a task")
-    print("2. Delete a task")   
-    print("3. View tasks")
-    print("4. Complete a task")
-    print("5. Exit")
+    while True:
+        print("1. Add a task")
+        print("2. Delete a task")   
+        print("3. View tasks")
+        print("4. Complete a task")
+        print("5. Exit")
 
-    user_choice = input("Enter your choice (1-5): ")
+        user_choice = input("Enter your choice (1-5): ")
 
-    
+        
 
-    if user_choice == '1':
-        task = input("Enter the task to add: ")
-        user_tasks.append(task)
-        print(f'Task "{task}" added.')
+        if user_choice == '1':
+            task = input("Enter the task to add: ")
+            user_tasks.append(task)
+            print(f'Task "{task}" added.')
 
-    elif user_choice == '2':    
-        task = input("Enter the task to delete: ")
-        if task in user_tasks:
-            user_tasks.remove(task)
-            print(f'Task "{task}" deleted.')
+        elif user_choice == '2':    
+            task = input("Enter the task to delete: ")
+            if task in user_tasks:
+                user_tasks.remove(task)
+                print(f'Task "{task}" deleted.')
+            else:
+                print(f'Task "{task}" not found.')
+
+        elif user_choice == '3':
+            if user_tasks:
+                print("Your tasks:")
+                for task in user_tasks:
+                    print(f'- {task}')
+            else:
+                print("No tasks found.")
+
+        elif user_choice == '4':
+            task = input("Enter the task to complete: ")
+            if task in user_tasks:
+                user_tasks.remove(task)
+                print(f'Task "{task}" completed.')
+            else:
+                print(f'Task "{task}" not found.')
+
+        elif user_choice == '5':
+            print("Exiting the application.")
+            break
+
         else:
-            print(f'Task "{task}" not found.')
+            print("Invalid choice. Please try again.")
 
-    elif user_choice == '3':
-        if user_tasks:
-            print("Your tasks:")
-            for task in user_tasks:
-                print(f'- {task}')
-        else:
-            print("No tasks found.")
 
-    elif user_choice == '4':
-        task = input("Enter the task to complete: ")
-        if task in user_tasks:
-            user_tasks.remove(task)
-            print(f'Task "{task}" completed.')
-        else:
-            print(f'Task "{task}" not found.')
-
-    elif user_choice == '5':
-        print("Exiting the application.")
-        break
-
-    else:
-        print("Invalid choice. Please try again.")
+if __name__ == "__main__":
+    main()
